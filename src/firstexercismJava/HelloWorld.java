c65b2ded5a25c172ea04984fb9f45881b6168858
Alexander <aleacosta@hacienda.gov.do>
2023-01-13 11:11:48 -0400

    HelloWorld

---
+++ src/firstexercismJava/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package firstexercismJava;
+
+/**
+ * @author jhosep
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String msg = getGreeting();
+		System.out.println(msg);
+	}
+
+	public static String getGreeting() {
+		return "Hello, World!";
+	}
+
+}
