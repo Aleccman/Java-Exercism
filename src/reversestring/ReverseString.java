b73928870e3ca4fa74253b6abd82408dc70c6aa5
Alexander <aleacosta@hacienda.gov.do>
2023-01-23 14:07:28 -0400

    Reverse String

---
+++ src/reversestring/ReverseString.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package reversestring;
+
+/**
+ * @author jhosep
+ *
+ */
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static String reverse(String inputString) {
+		return new StringBuilder(inputString).reverse().toString();
+
+	}
+
+}
---
+++ src/twofer/TwoFer.java
@@ -0,0 +1,22 @@
+package twofer;
+/**
+ * 
+ */
+
+/**
+ * @author jhosep
+ *
+ */
+public class TwoFer {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static String twofer(String name) {
+
+		return name != null ? "One for " + name + ", one for me." : "One for you, one for me.";
+
+	}
+
+}
