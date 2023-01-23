d6a3778cae107fe7fd90c38593fa4d71dc6dd46a
Alexander <aleacosta@hacienda.gov.do>
2023-01-23 11:34:51 -0400

    LogLeves Exercism

---
+++ src/loglevels/LogLevels.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package loglevels;
+
+/**
+ * @author jhosep
+ *
+ */
+public class LogLevels {
+
+	public static void main(String[] args) {
+
+//		String value = ("[ERROR]: Invalid operation");
+//		String value2 = ("[WARNING]:  Disk almost full\r\n");
+		String value3 = ("[INFO]: Operation completed");
+
+		String valuefour = reformat(value3);
+		System.out.println(valuefour);
+
+	}
+
+	public static String message(String logLine) {
+		return logLine.substring(logLine.indexOf(":") + 1).trim();
+
+	}
+
+	public static String logLevel(String logLine) {
+		return logLine.substring(1, logLine.indexOf(":") - 1).toLowerCase();
+	}
+
+	public static String reformat(String logLine) {
+//		return message(logLine.substring(logLine.indexOf(":") + 1)
+//				.concat(" " + logLevel(logLine)));
+		return String.format("%s (%s)", message(logLine),logLevel(logLine));
+
+	}
+
+}
---
+++ src/test/Test.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author jhosep
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		String myStr = "Hello planet earth, you are a great planet.";
+
+	}
+
+}
