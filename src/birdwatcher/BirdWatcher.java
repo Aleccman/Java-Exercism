8ca03819684c7bc46a4c8f1913e241ee5dd48c4f
Alexander <aleacosta@hacienda.gov.do>
2023-01-26 13:50:09 -0400

    BirdWatcher

---
+++ src/birdwatcher/BirdWatcher.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package birdwatcher;
+
+/**
+ * @author jhosep
+ *
+ */
+public class BirdWatcher {
+
+	public static void main(String[] args) {
+//		int [] twoInts = new int 2;
+
+	}
+
+	public static int[] getLastWeek() {
+		throw new UnsupportedOperationException("Please implement the BirdCount.getLastWeek() method");
+	}
+
+	public static int getToday() {
+		throw new UnsupportedOperationException("Please implement the BirdCount.getToday() method");
+	}
+
+	public static void incrementTodaysCount() {
+		throw new UnsupportedOperationException("Please implement the BirdCount.incrementTodaysCount() method");
+	}
+
+	public static boolean hasDayWithoutBirds() {
+		throw new UnsupportedOperationException("Please implement the BirdCount.hasDayWithoutBirds() method");
+	}
+
+	public static int getCountForFirstDays(int numberOfDays) {
+		throw new UnsupportedOperationException("Please implement the BirdCount.getCountForFirstDays() method");
+	}
+
+	public static int getBusyDays() {
+		throw new UnsupportedOperationException("Please implement the BirdCount.getBusyDays() method");
+	}
+
+}
